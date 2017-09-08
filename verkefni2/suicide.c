0282cdcbcec9ac0c03d24d1ce402655b18f11c17
Jóel Snær Garcia <joelga239@nemi.tskoli.is>
2017-09-08 09:06:41 +0000

    klarad suicide

--- verkefni2/suicide.c
+++ verkefni2/suicide.c
@@ -4,12 +4,26 @@
 
 task main()
 {
-
+	double counter = 1;
+	while(counter < 6 ){
 		motor[rightMotor] = 127;
 		motor[leftMotor]	= 127;
-		wait1Msec(5000);
+		wait1Msec(1000 * counter);
 
+		motor[leftMotor]	= 0;
+		motor[rightMotor] = 0;
+		wait1Msec(1000);
 
+		motor[rightMotor] = -127;
+		motor[leftMotor]	= -127;
+		wait1Msec(1000 * counter);
+
+		motor[leftMotor]	= 0;
+		motor[rightMotor] = 0;
+		wait1Msec(1000);
+
+		counter += 1;
+	}
 
 
 }
