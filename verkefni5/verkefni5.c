4ee3e9b1384ef361f6412efe8bf8ef53e80d2c70
Mikael Máni Jónsson <MikaelMani99@users.noreply.github.com>
2017-11-17 10:06:32 +0000

    Breytt í ensk heiti

--- verkefni5/verkefni5.c
+++ verkefni5/verkefni5.c
@@ -1,7 +1,11 @@
-#pragma config(Sensor, dgtl1,  rightEncoder,        sensorQuadEncoder)
-#pragma config(Sensor, dgtl3,  leftEncoder,         sensorQuadEncoder)
+#pragma config(Sensor, dgtl3,  rightEncoder,        sensorQuadEncoder)
+#pragma config(Sensor, dgtl1,  leftEncoder,         sensorQuadEncoder)
+#pragma config(Sensor, in6,    potentiometer,   sensorPotentiometer)
+#pragma config(Sensor, dgtl8,  sonarSensor,         sensorSONAR_cm)
 #pragma config(Motor,  port2,           rightMotor,    tmotorServoContinuousRotation, openLoop)
 #pragma config(Motor,  port3,           leftMotor,     tmotorServoContinuousRotation, openLoop, reversed)
+#pragma config(Motor,  port7,           armMotor,     tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor, port5, 						clawMotor, 		tmotorServoContinuousRotation, openLoop)
 #pragma config(Sensor, in3,    lineFollowerRIGHT,   sensorLineFollower)
 #pragma config(Sensor, in2,    lineFollowerCENTER,  sensorLineFollower)
 #pragma config(Sensor, in1,    lineFollowerLEFT,    sensorLineFollower)
@@ -13,7 +17,7 @@ is darker and drives around the line
 #include "../headers/vex.h"
 #include "../robotfunctions/functions.c"
 
-task keyraEftirLinu() {
+task followLine() {
 	while(true)
 	{
 		int threshold = 2500;
@@ -39,7 +43,7 @@ task keyraEftirLinu() {
 
 task main()
 {
-	StartTask(keyraEftirLinu);
+	StartTask(followLine);
 	while(vexRT[Btn7D] != 1){
 	}
 	StopAllTasks();
